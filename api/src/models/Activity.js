308414d560e9c68d5b14cfe85bf12c95cb2399a3
oskarp88 <oskyburgos81@gmail.com>
2022-12-09 14:57:25 -0500

    modificando

---
+++ api/src/models/Activity.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize")
+
+
+module.exports = (sequelize)=>{
+    sequelize.define("activity",{
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        difficulty:{
+            type: DataTypes.ENUM('1','2','3','4','5'),
+            allowNull: false,
+        },
+        duration:{
+            type: DataTypes.INTEGER,
+            allowNull:false,
+        },
+        season:{
+           type: DataTypes.ENUM('Verano','Otoño','Invierno','Primavera'),
+            allowNull:false,
+        },
+
+    },
+    {
+        timestamps: false,
+      })
+}
